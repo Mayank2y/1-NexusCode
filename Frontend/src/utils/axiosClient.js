fc486e5720669b7cd1ce7e22418bdd61ef508787
Saurabh2y <mayanktiwari88777@gmail.com>
2026-06-14 23:44:32 +0530

    final api commit

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+    baseURL: 'http://13.203.66.112',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
